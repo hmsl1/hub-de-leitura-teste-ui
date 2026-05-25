5eb02e435fa2ea7ba5c4231fef7534dff5670430
hugo <hugomsl459@gmail.com>
2026-05-25 15:02:13 -0300

    automação do contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,54 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('index.html')
+  })
+  it('Deve preencher formulário de contato com sucesso', () => {
+    cy.get('[name="name"]').type('Fabio Araujo')
+    cy.get('[name="email"]').type('fabio@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Olá, gostaria de saber mais sobre as parcerias.')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    cy.get('[name="name"]').clear()
+    cy.get('[name="email"]').type('fabio@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Olá, gostaria de saber mais sobre as parcerias.')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('[name="name"]').type('Fabio Araujo')
+    cy.get('[name="email"]').clear()
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Olá, gostaria de saber mais sobre as parcerias.')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher assunto', () => {
+    cy.get('[name="name"]').type('Fabio Araujo')
+    cy.get('[name="email"]').type('fabio@teste.com')
+    cy.get('[name="message"]').type('Olá, gostaria de saber mais sobre as parcerias.')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto.')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher mensagem', () => {
+    cy.get('[name="name"]').type('Fabio Araujo')
+    cy.get('[name="email"]').type('fabio@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').clear()
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem.')
+  });
+});
