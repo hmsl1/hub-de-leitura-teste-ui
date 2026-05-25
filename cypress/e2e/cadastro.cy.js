a0900e182d8deb60d1bdb93e00a90ddee107bf81
hugo <hugomsl459@gmail.com>
2026-05-25 16:21:57 -0300

    automação da tela da lista de catálogo

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('register.html')
+    })
+
+    it('Deve fazer cadastro com sucesso, usando função JS', () => {
+        let email = `teste${Date.now()}@teste.com`
+        cy.get('#name').type('Fabio Araujo')
+        cy.get('#email').type(email)
+        cy.get('#phone').type('11999999999')
+        cy.get('#password').type('Senha123')
+        cy.get('#confirm-password').type('Senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        cy.url().should('include', 'dashboard.html')
+    })
+
+    it('Deve fazer cadastro com sucesso, usando Faker', () => {
+        let nome = faker.person.fullName()
+        let email = faker.internet.email()
+        cy.get('#name').type(nome)
+        cy.get('#email').type(email)
+        cy.get('#phone').type('11999999999')
+        cy.get('#password').type('Senha123')
+        cy.get('#confirm-password').type('Senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        cy.url().should('include', 'dashboard.html')
+        cy.get('#user-name').should('contain', nome)
+    })
+
+});
